262bf8992dca9c5dfccbcd576fad9300e0eb66ec
BOSONGO <celestin.mombela@yahoo.fr>
2022-05-21 06:01:24 +0200

    Update PersonRepository.php

--- src/Repository/PersonRepository.php
+++ src/Repository/PersonRepository.php
@@ -19,32 +19,18 @@ class PersonRepository extends ServiceEntityRepository
         parent::__construct($registry, Person::class);
     }
 
-    // /**
-    //  * @return Person[] Returns an array of Person objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+      * @return Person[] Returns an array of Person objects
+     */
+
+    public function findAllByUserClient()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
+            ->andWhere('p.userClient > :now')
+            ->orderBy('p.userClient', 'ASC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Person
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
